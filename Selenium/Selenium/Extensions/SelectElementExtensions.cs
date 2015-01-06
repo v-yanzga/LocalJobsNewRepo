11c7031894742734bb210dd8563bd3dadbac6666
v-weiqi-beyondsoft <v-weiqi@microsoft.com>
2015-01-06 00:04:50 -0800

    extentions

---
+++ Selenium/Selenium/Extensions/SelectElementExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+
+namespace Selenium.Extensions
+{
+    public static class SelectElementExtensions
+    {
+        public static void SelectByText(this SelectElement selectElement,string text,int timePreExecute = 500)
+        {
+            try
+            {
+                Thread.Sleep(timePreExecute);
+                selectElement.SelectByText(text);
+            }
+            catch (NoSuchElementException)
+            {
+                throw;
+            }
+            catch
+            {
+                SelectByText(selectElement, text, timePreExecute);
+            }
+        }
+    }
+}
---
+++ Selenium/Selenium/Extensions/WebDriverExtensions.cs
@@ -0,0 +1,48 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium.Extensions
+{
+    public static class WebDriverExtensions
+    {
+        public static void WaitForPageToLoad(this IWebDriver driver, int seconds = 30)
+        {
+            TimeSpan timeout = TimeSpan.FromSeconds(seconds);
+            WebDriverWait wait = new WebDriverWait(driver, timeout);
+
+            IJavaScriptExecutor javascript = driver as IJavaScriptExecutor;
+            if (javascript == null)
+            {
+                throw new ArgumentException("driver", "Driver must support javascript execution");
+            }
+
+            wait.Until((d) =>
+            {
+                try
+                {
+                    string readyState = javascript.ExecuteScript("if (document.readyState) return document.readyState;").ToString();
+                    return readyState.ToLower() == "complete";
+                }
+                catch (InvalidOperationException e)
+                {
+                    // Window is no longer available
+                    return e.Message.ToLower().Contains("unable to get browser");
+                }
+                catch (WebDriverException e)
+                {
+                    // Browser is no longer available
+                    return e.Message.ToLower().Contains("unable to connect");
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            });
+        }
+    }
+}
