8741d2ef7f32c63cc15652cfe62017ca0b23b1d5
v-weiqi-beyondsoft <v-weiqi@microsoft.com>
2015-01-05 19:05:01 -0800

    structure

---
+++ Selenium/ConsoleApplication/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Selenium.BrowserMatrix;
+using OpenQA.Selenium;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = Chrome.ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(@"http://www.google.com");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Selenium/Selenium/BrowserMatrix/Chrome.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium.BrowserMatrix
+{
+    public class Chrome
+    {
+        public static ChromeDriver ChromeDriver()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("test-type");
+            ChromeDriverService CDService = ChromeDriverService.CreateDefaultService();
+            return new ChromeDriver(CDService, options);
+        }
+    }
+}
