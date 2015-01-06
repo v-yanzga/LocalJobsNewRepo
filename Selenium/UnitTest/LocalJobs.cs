d8a6743e797b1b8c44915573c2b723a0bf194111
v-weiqi-beyondsoft <v-weiqi@microsoft.com>
2015-01-05 21:38:29 -0800

    test structure

---
+++ Selenium/UnitTest/LocalJobs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class LocalJobs
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //TODO: your test code goes here:
+        }
+    }
+}
